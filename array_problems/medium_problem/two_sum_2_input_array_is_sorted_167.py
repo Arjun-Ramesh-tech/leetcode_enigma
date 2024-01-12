a894bc8a56dfee8f099de2909781781f560ac740
Arjun-Ramesh-tech <arjun.arjrames@gmail.com>
2024-01-11 18:38:13 -0700

    167

---
+++ array_problems/medium_problem/two_sum_2_input_array_is_sorted_167.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(numbers)):
+            if numbers[i] in hashmap:
+                return [hashmap[numbers[i]]+1,i+1]
+            hashmap[target-numbers[i]] = i
