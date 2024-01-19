081c8dfc88e3c2f210e2ec16aa539d5dc6d6fe57
Arjun-Ramesh-tech <arjun.arjrames@gmail.com>
2024-01-19 02:12:29 -0700

    2390

---
+++ stack/medium/remove_stars_from_a_string_2390.py
@@ -0,0 +1,6 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+        for i in s:
+            stack.pop() if i =='*'  else stack.append(i)
+        return''.join(stack)
