a19432cff16758f4371b9ff4f2e0bb85b1224cba
Arjun-Ramesh-tech <arjun.arjrames@gmail.com>
2024-03-28 03:53:21 -0700

    1762

---
+++ stack/building_with_ocean_view_1762.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        n = len(heights)
+        answer = []
+        for i in range(n):
+            while answer and  heights[answer[-1]]<=heights[i]:
+                answer.pop()
+            answer.append(i)
+        return answer
+        
